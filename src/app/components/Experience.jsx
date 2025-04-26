54947e728010780f5506e52ffbb9272e92d20d8e
artgoblin <satabdadas3@gmail.com>
2025-04-26 15:40:41 +0530

    chevronright theme adjustment

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -36,17 +36,36 @@ const Experience = () => {
         <AccordionItem value="ibs-software" className="py-2">
           <AccordionTrigger className="w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50">
             <div className="flex items-center">
-              <ChevronRight className="h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50" />
+              <ChevronRight
+                className={`h-4 w-4 ${
+                  isDarkMode ? "text-white" : "text-black"
+                } transition-transform duration-200 group-data-expanded:rotate-90`}
+              />
               <div className="flex items-center justify-between w-full">
-                <div className={`font-semibold text-lg ${isDarkMode ? "text-white" : "text-black"}`}>IBS Software
-                <em className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} px-3`}>
+                <div
+                  className={`font-semibold text-lg ${
+                    isDarkMode ? "text-white" : "text-black"
+                  }`}
+                >
+                  IBS Software
+                  <em
+                    className={`text-sm ${
+                      isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                    } px-3`}
+                  >
                     (Full-Time Employee)
                   </em>
                 </div>
-                <div className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} flex items-center space-x-4`}>
+                <div
+                  className={`text-sm ${
+                    isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                  } flex items-center space-x-4`}
+                >
                   <em>Jun 2023 – Present</em>
                   <a
-                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${isDarkMode ? "text-white" : "text-black"} border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
+                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${
+                      isDarkMode ? "text-white" : "text-black"
+                    } border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
                     onClick={() =>
                       window.open(
                         "https://www.linkedin.com/feed/update/urn:li:activity:7230129443753615361/",
@@ -61,7 +80,11 @@ const Experience = () => {
             </div>
           </AccordionTrigger>
           <AccordionContent className="origin-left">
-            <ul className={`pl-6 pr-2 ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} list-disc space-y-2`}>
+            <ul
+              className={`pl-6 pr-2 ${
+                isDarkMode ? "text-zinc-300" : "text-zinc-800"
+              } list-disc space-y-2`}
+            >
               <li>
                 Developed and deployed 10+ features for an airline SaaS
                 platform, reducing bugs by 15% through rigorous testing and
@@ -87,24 +110,38 @@ const Experience = () => {
         <AccordionItem value="cognizant" className="py-2">
           <AccordionTrigger className="w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50">
             <div className="flex items-center">
-              <ChevronRight className="h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50" />
+              <ChevronRight
+                className={`h-4 w-4 ${
+                  isDarkMode ? "text-white" : "text-black"
+                } transition-transform duration-200 group-data-expanded:rotate-90`}
+              />
+
               <div className="flex ml-2 items-center justify-between w-full">
-                <div className={`font-semibold text-lg ${isDarkMode ? "text-white" : "text-black"}`}>
+                <div
+                  className={`font-semibold text-lg ${
+                    isDarkMode ? "text-white" : "text-black"
+                  }`}
+                >
                   Cognizant Technology Solutions
-                  <em className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} px-3`}>
+                  <em
+                    className={`text-sm ${
+                      isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                    } px-3`}
+                  >
                     (Internship)
                   </em>
                 </div>
-                <div className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} flex items-center space-x-4`}>
+                <div
+                  className={`text-sm ${
+                    isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                  } flex items-center space-x-4`}
+                >
                   <em>Jan 2023 – Apr 2023</em>
                   <a
-                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${isDarkMode ? "text-white" : "text-black"} border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
-                    onClick={() =>
-                      window.open(
-                        "/cognizant.pdf",
-                        "_blank"
-                      )
-                    }
+                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${
+                      isDarkMode ? "text-white" : "text-black"
+                    } border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
+                    onClick={() => window.open("/cognizant.pdf", "_blank")}
                   >
                     <FaExternalLinkAlt className="h-4 w-4 mr-1" />
                   </a>
@@ -113,7 +150,11 @@ const Experience = () => {
             </div>
           </AccordionTrigger>
           <AccordionContent className="origin-left">
-            <ul className={`pl-6 pr-2 ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} list-disc space-y-2`}>
+            <ul
+              className={`pl-6 pr-2 ${
+                isDarkMode ? "text-zinc-300" : "text-zinc-800"
+              } list-disc space-y-2`}
+            >
               <li>
                 Designed and optimized data models using Snowflake and SQL
                 Server, improving query performance by 20%.
@@ -134,24 +175,38 @@ const Experience = () => {
         <AccordionItem value="highradius" className="py-2">
           <AccordionTrigger className="w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50">
             <div className="flex items-center">
-              <ChevronRight className="h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50" />
+              <ChevronRight
+                className={`h-4 w-4 ${
+                  isDarkMode ? "text-white" : "text-black"
+                } transition-transform duration-200 group-data-expanded:rotate-90`}
+              />
+
               <div className="flex ml-2 items-center justify-between w-full">
-                <div className={`font-semibold text-lg ${isDarkMode ? "text-white" : "text-black"}`}>
+                <div
+                  className={`font-semibold text-lg ${
+                    isDarkMode ? "text-white" : "text-black"
+                  }`}
+                >
                   HighRadius
-                  <em className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} px-3`}>
+                  <em
+                    className={`text-sm ${
+                      isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                    } px-3`}
+                  >
                     (Winter Internship)
                   </em>
                 </div>
-                <div className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} flex items-center space-x-4`}>
+                <div
+                  className={`text-sm ${
+                    isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                  } flex items-center space-x-4`}
+                >
                   <em>Jan 2022 – Apr 2022</em>
                   <a
-                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${isDarkMode ? "text-white" : "text-black"} border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
-                    onClick={() =>
-                      window.open(
-                        "/highradius.pdf",
-                        "_blank"
-                      )
-                    }
+                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${
+                      isDarkMode ? "text-white" : "text-black"
+                    } border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
+                    onClick={() => window.open("/highradius.pdf", "_blank")}
                   >
                     <FaExternalLinkAlt className="h-4 w-4 mr-1" />
                   </a>
@@ -160,7 +215,11 @@ const Experience = () => {
             </div>
           </AccordionTrigger>
           <AccordionContent className="origin-left">
-            <ul className={`pl-6 pr-2 ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} list-disc space-y-2`}>
+            <ul
+              className={`pl-6 pr-2 ${
+                isDarkMode ? "text-zinc-300" : "text-zinc-800"
+              } list-disc space-y-2`}
+            >
               <li>
                 Developed and test b2c web application using react and python.
               </li>
@@ -177,24 +236,38 @@ const Experience = () => {
         <AccordionItem value="pearlthoughts" className="py-2">
           <AccordionTrigger className="w-full py-0.5 text-left text-zinc-950 dark:text-zinc-50">
             <div className="flex items-center">
-              <ChevronRight className="h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-expanded:rotate-90 dark:text-zinc-50" />
+              <ChevronRight
+                className={`h-4 w-4 ${
+                  isDarkMode ? "text-white" : "text-black"
+                } transition-transform duration-200 group-data-expanded:rotate-90`}
+              />
+
               <div className="flex ml-2 items-center justify-between w-full">
-                <div className={`font-semibold text-lg ${isDarkMode ? "text-white" : "text-black"}`}>
+                <div
+                  className={`font-semibold text-lg ${
+                    isDarkMode ? "text-white" : "text-black"
+                  }`}
+                >
                   PearlThoughts
-                  <em className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} px-3`}>
+                  <em
+                    className={`text-sm ${
+                      isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                    } px-3`}
+                  >
                     (Summer Internship)
                   </em>
                 </div>
-                <div className={`text-sm ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} flex items-center space-x-4`}>
+                <div
+                  className={`text-sm ${
+                    isDarkMode ? "text-zinc-300" : "text-zinc-800"
+                  } flex items-center space-x-4`}
+                >
                   <em>Aug 2021 – Oct 2021</em>
                   <a
-                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${isDarkMode ? "text-white" : "text-black"} border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
-                    onClick={() =>
-                      window.open(
-                        "pearlthought.pdf",
-                        "_blank"
-                      )
-                    }
+                    className={`relative rounded-full cursor-pointer p-1 mr-2 ${
+                      isDarkMode ? "text-white" : "text-black"
+                    } border-1 border-transparent hover:border-pink-500 hover:shadow-[0_0_1px_1px] hover:shadow-pink-500 transition-all duration-300`}
+                    onClick={() => window.open("pearlthought.pdf", "_blank")}
                   >
                     <FaExternalLinkAlt className="h-4 w-4 mr-1" />
                   </a>
@@ -203,7 +276,11 @@ const Experience = () => {
             </div>
           </AccordionTrigger>
           <AccordionContent className="origin-left">
-            <ul className={`pl-6 pr-2 ${isDarkMode ? "text-zinc-300" : "text-zinc-800"} list-disc space-y-2`}>
+            <ul
+              className={`pl-6 pr-2 ${
+                isDarkMode ? "text-zinc-300" : "text-zinc-800"
+              } list-disc space-y-2`}
+            >
               <li>Boiler plate code generation using react and Java.</li>
               <li>performance enhancement of the overall application.</li>
               <li>Optimoization of the code base.</li>
